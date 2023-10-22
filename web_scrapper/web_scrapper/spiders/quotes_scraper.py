5f56292fe3dc4ebdf3208a216ccd5863063d3eae
Ad450 <adjeimanue@gmail.com>
2023-10-22 17:51:22 +0000

    fix crawler

--- web_scrapper/web_scrapper/spiders/quotes_scraper.py
+++ web_scrapper/web_scrapper/spiders/quotes_scraper.py
@@ -1,30 +1,27 @@
 
+"""
+Simple Quotes scraper
+"""
+
+from typing import Any, Iterable
 from scrapy import Spider
 from scrapy.http import Request, Response
-from typing import Iterable
 from web_scrapper.items import WebScrapperItem
+class QuotesScraper(Spider):
 
-class QuotesScraper (Spider):  
     name:str = "quotes"
-    urls= ["https://quotes.toscrape.com/page/1/",
+    urls= ["https://quotes.toscrape.com/page/1",
         "https://quotes.toscrape.com/page/2/" ]
     
     #override
     def start_requests (self) -> Iterable[Request] :
         for url in self.urls:
-           yield  Request(url=url, callback=self.parse)
+            yield  Request(url=url, callback=self.parse)
 
-    def parse(self, response:Response) -> Iterable[any] :
+    def parse(self, response: Response, **kwargs: Any) -> Any:
         for quote in response.css("div.quote") :
              yield WebScrapperItem(
                  text = quote.css("span.text::text").get(),
                  author =  quote.css("small.author::text").get(),
                  tags = quote.css("div.tags a.tag::text").getall()
-             )     
-               
-               
-             
-       
-
-
-
+             )
