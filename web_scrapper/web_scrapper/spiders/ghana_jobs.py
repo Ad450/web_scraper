5ad94dbe079e607ec4213c11bd0b241f08efc9ab
Ad450 <adjeimanue@gmail.com>
2023-11-20 20:56:42 +0000

    scrape job and company details. more refinement upnext

--- web_scrapper/web_scrapper/spiders/ghana_jobs.py
+++ web_scrapper/web_scrapper/spiders/ghana_jobs.py
@@ -150,8 +150,8 @@ class GhanaJobsScraper(Spider):
         if company_title_node is None:
             raise ContentNotFoundException(message="company_title_node not found")
 
-        # company_title_link = GHANA_JOBS_URL.rstrip("/") + company_title_node["href"]
-        # company_title = company_title_node.text
+        company_title_link = GHANA_JOBS_URL.rstrip("/") + company_title_node["href"]
+        company_title = company_title_node.text
 
         company_logo_node = job_node.find(name="div", class_="company-logo-mobile")
 
@@ -159,6 +159,117 @@ class GhanaJobsScraper(Spider):
             raise ContentNotFoundException(message="company_logo_node not found")
 
         company_logo_image = company_logo_node.find(name="img")
-        if company_logo_node is None:
-            raise ContentNotFoundException(message="company_logo_node not found")
+        if company_logo_image is None:
+            company_logo_image = None
         # print(company_logo_image["src"])
+
+        company_info_node = company_profile_node.find(
+            name="table", class_="company-info"
+        )
+        if company_info_node is None:
+            raise ContentNotFoundException(message="company_info_node not found")
+        website_label = company_info_node.find(name="td", class_="website-label")
+        website_url_node = company_info_node.find(name="td", class_="website-url")
+
+        website_url = (
+            website_url_node.find(name="a")["href"]
+            if website_url_node is not None
+            else None
+        )
+
+        sector_label = company_info_node.find(name="td", class_="sector-label")
+        sector_title = company_info_node.find(name="td", class_="sector-title")
+
+        if sector_title is None:
+            raise ContentNotFoundException(message="sector_title not found")
+        fields = []
+        for sector_field in sector_title.find_all(name="div", class_="field-item"):
+            fields.append(sector_field.text)
+
+        job_ad_details_node = job_node.find(name="div", id="job-ad-details-" + job_id)
+        job_title_node = job_ad_details_node.find(name="span", class_="ad-ss-title")
+        job_description_node = job_ad_details_node.find_all(name="p")
+
+        descriptions = []
+        job_description = ""
+        for description in job_description_node:
+            descriptions.append(description.text)
+
+        job_description = job_description.join(descriptions)
+
+        job_criteria = {}
+        job_criteria_node = job_node.find(name="table", class_="job-ad-criteria")
+        if job_criteria_node is None:
+            raise ContentNotFoundException(message="job_criteria_node not found")
+
+        for criteria in job_criteria_node.find_all(name="tr"):
+            criteria_key = criteria.find_all(name="td")[0]
+            criteria_value_node = criteria.find_all(name="td")[1]
+            criteria_value = ""
+            if criteria_value_node is None:
+                criteria_value_node = None
+            else:
+                criteria_value_field = criteria_value_node.find(
+                    name="div", class_="field-item"
+                )
+                if criteria_value_field is None:
+                    criteria_value = None
+                else:
+                    criteria_value = criteria_value_field.get_text(strip=True)
+
+            if criteria_key is not None and criteria_value is not None:
+                key = criteria_key.get_text(strip=True).replace(":", "")
+                job_criteria[key] = criteria_value
+
+        yield Request(
+            url=company_title_link,
+            callback=self.__parse_company_description,
+            headers=headers,
+            meta={
+                "job_details": {
+                    "company-title-link": company_title_link,
+                    "company-title": company_title,
+                    "company-image-url": company_logo_image["src"]
+                    if company_logo_image is not None
+                    else None,
+                    "company-website-label": website_label.text
+                    if website_label is not None
+                    else None,
+                    "company-website-url": website_url,
+                    "company-sector": sector_label.text,
+                    "company-sector-fields": fields,
+                    "job-title": job_title_node.text
+                    if job_title_node is not None
+                    else None,
+                    "job-description": job_description,
+                },
+                "job_criteria": job_criteria,
+            },
+        )
+
+    def __parse_company_description(
+        self,
+        response: Response,
+    ) -> str:
+        bs: BeautifulSoup = self.get_beautiful_soup(markup=response.text, parser="lxml")
+        company_profile_description = bs.find(
+            name="div", class_="company-profile-description"
+        )
+        if company_profile_description is None:
+            raise ContentNotFoundException(message="sector_title not found")
+
+        company_description_node = company_profile_description.find_all(name="p")
+        company_description = ""
+        if company_description_node is None or len(company_description_node) == 0:
+            company_description_node = None
+        else:
+            descriptions = []
+            for description in company_description_node:
+                descriptions.append(description.text)
+            company_description = company_description.join(descriptions)
+
+        yield {
+            **response.meta["job_details"],
+            **response.meta["job_criteria"],
+            "company-description": company_description,
+        }
